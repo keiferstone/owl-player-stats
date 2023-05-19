0f8dfd4f346d09807ceb230ac6319e2c51dc6661
Keifer Stone <keiferstone@gmail.com>
2023-05-19 01:03:08 -0600

    dark theme support

--- app/src/main/java/com/keiferstone/owlplayerstats/activity/MainActivity.kt
+++ app/src/main/java/com/keiferstone/owlplayerstats/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -24,43 +25,49 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
 
             AppTheme {
-                NavHost(
-                    navController = navController,
-                    startDestination = "player-grid") {
-                    composable("player-grid") {
-                        PlayerGridScreen(
-                            onPlayerSelected =  { player ->
-                                navController.navigate("player-detail/${player.id}")
-                            },
-                            onPlayerPairSelected = { first, second ->
-                                navController.navigate("player-comparison/${first.id}/${second.id}")
-                            },
-                            onStatsSelected = {
-                                navController.navigate("stats-list")
-                            },
-                        )
-                    }
-                    composable(
-                        route = "player-detail/{playerId}",
-                        arguments = listOf(navArgument("playerId") { type = NavType.LongType })) { backStackEntry ->
-                        backStackEntry.arguments?.getLong("playerId")?.let {
-                            PlayerDetailScreen(it)
+                Surface {
+                    NavHost(
+                        navController = navController,
+                        startDestination = "player-grid"
+                    ) {
+                        composable("player-grid") {
+                            PlayerGridScreen(
+                                onPlayerSelected = { player ->
+                                    navController.navigate("player-detail/${player.id}")
+                                },
+                                onPlayerPairSelected = { first, second ->
+                                    navController.navigate("player-comparison/${first.id}/${second.id}")
+                                },
+                                onStatsSelected = {
+                                    navController.navigate("stats-list")
+                                },
+                            )
                         }
-                    }
-                    composable(
-                        route = "player-comparison/{player1Id}/{player2Id}",
-                        arguments = listOf(
-                            navArgument("player1Id") { type = NavType.LongType },
-                            navArgument("player2Id") { type = NavType.LongType })) { backStackEntry ->
-                        val player1Id = backStackEntry.arguments?.getLong("player1Id")
-                        val player2Id = backStackEntry.arguments?.getLong("player2Id")
-                        if (player1Id != null && player2Id != null) {
-                            PlayerComparisonScreen(player1Id, player2Id)
+                        composable(
+                            route = "player-detail/{playerId}",
+                            arguments = listOf(navArgument("playerId") { type = NavType.LongType })
+                        ) { backStackEntry ->
+                            backStackEntry.arguments?.getLong("playerId")?.let {
+                                PlayerDetailScreen(it)
+                            }
                         }
-                    }
-                    composable(
-                        route = "stats-list") {
-                        StatListScreen()
+                        composable(
+                            route = "player-comparison/{player1Id}/{player2Id}",
+                            arguments = listOf(
+                                navArgument("player1Id") { type = NavType.LongType },
+                                navArgument("player2Id") { type = NavType.LongType })
+                        ) { backStackEntry ->
+                            val player1Id = backStackEntry.arguments?.getLong("player1Id")
+                            val player2Id = backStackEntry.arguments?.getLong("player2Id")
+                            if (player1Id != null && player2Id != null) {
+                                PlayerComparisonScreen(player1Id, player2Id)
+                            }
+                        }
+                        composable(
+                            route = "stats-list"
+                        ) {
+                            StatListScreen()
+                        }
                     }
                 }
             }
